4ab1b6d79a7f476a8895ba5cc9bc2a1ce0d17451
ahadddd <ahad.shehbaz19@gmail.com>
2023-05-30 17:35:51 +0500

    Voter section integrated

--- src/app/Main/Admin/Voter/Voter.component.ts
+++ src/app/Main/Admin/Voter/Voter.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-Voter',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoterComponent implements OnInit {
 
-  constructor() { }
+  baseUrl = `https://localhost:7056/api/`;
+  voters: any = [];
+  createModal: boolean = false;
+  createForm!: FormGroup;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.createForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      city: new FormControl('', Validators.required),
+    })
+  }
+
+  getVoters() {
+    let req = this.http.get<any>(this.baseUrl + 'Voter');
+    req.subscribe({
+      next: (data) => this.voters = data,
+      error: (err) => console.log(err)
+    })
+  }
+
+  addVoter() {
+    if (this.createModal) {
+      if (this.createForm.invalid) {
+        alert('invalid data entered.')
+      }
+      this.createModal = !this.createModal;
+    }
+    else {
+      this.createModal = !this.createModal;
+    }
   }
 
 }
