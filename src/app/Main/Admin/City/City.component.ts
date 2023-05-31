6c61cb462df7424a92038ca26c0a530643916d88
ahadddd <ahad.shehbaz19@gmail.com>
2023-05-31 12:03:03 +0500

    CRUD Operations for City integrated to frontend

--- src/app/Main/Admin/City/City.component.ts
+++ src/app/Main/Admin/City/City.component.ts
@@ -11,6 +11,10 @@ export class CityComponent implements OnInit {
   baseUrl = `https://localhost:7056/api/City`;
   cities: any = [];
   createForm !: FormGroup;
+  updateForm !: FormGroup;
+  createModal: boolean = false;
+  updateModal: boolean = false;
+  globalHolder: any;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
@@ -24,8 +28,67 @@ export class CityComponent implements OnInit {
     req.subscribe({
       next: (data) => this.cities = data,
       error: (err) => console.log(err)
-      
-    })
+    });
+  }
+
+  createCity() {
+    if (this.createModal) {
+      if (this.createForm.invalid) {
+        alert('Invalid data provided.');
+      }
+      else {
+        let ctrl = this.createForm.controls;
+        let c1 = {
+          name: ctrl['name'].getRawValue()
+        };
+        let req = this.http.post(this.baseUrl, c1);
+        req.subscribe({
+          next: (res) => console.log(res),
+          error: (err) => console.log(err)
+        });
+      }
+      this.createModal = !this.createModal;
+    }
+    else {
+      this.createModal = !this.createModal;
+    }
+  }
+
+  updateCity(i?: any) {
+    if (this.updateModal) {
+      if (this.updateForm.invalid) {
+        alert('Invalid data provided.');
+      }
+      else {
+        let ctrl = this.updateForm.controls;
+        let c1 = {
+          newName: ctrl['newName'].getRawValue()
+        };
+        let req = this.http.put(this.baseUrl + `/${this.cities[this.globalHolder]?.name}`, c1);
+        req.subscribe({
+          next: (res) => console.log(res),
+          error: (err) => console.log(err)
+        });
+        this.globalHolder = null;
+      }
+      this.updateModal = !this.updateModal;
+    }
+    else {
+      this.updateModal = !this.updateModal;
+      this.updateForm = new FormGroup({
+        name: new FormControl(`${this.cities[i].name}`),
+        newName: new FormControl('', Validators.required)
+      });
+      this.globalHolder = i;
+    }
+  }
+
+  deleteCity(i: any) {
+    let req = this.http.delete(this.baseUrl+`/${this.cities[i].name}`);
+    req.subscribe({
+      next: (res) => console.log(res),
+      error: (err) => console.log(err)
+    });
   }
 
 }
