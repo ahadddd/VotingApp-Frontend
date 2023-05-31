9df681252f8a091c76286f855cfd608b8f668951
ahadddd <ahad.shehbaz19@gmail.com>
2023-05-31 13:42:12 +0500

    Candidate component for User View

---
+++ src/app/Main/User/Candidate/Candidate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Candidate',
+  templateUrl: './Candidate.component.html',
+  styleUrls: ['./Candidate.component.css']
+})
+export class CandidateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
