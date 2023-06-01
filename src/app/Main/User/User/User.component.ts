1a69ca057bef2ccb63fe38b1c6fcc15fb6164643
ahadddd <ahad.shehbaz19@gmail.com>
2023-06-01 18:39:36 +0500

    Vote entity created and integrated with backend

--- src/app/Main/User/User/User.component.ts
+++ src/app/Main/User/User/User.component.ts
@@ -1,25 +1,92 @@
-import { Voter } from './../../../../Models/Voter';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-User',
   templateUrl: './User.component.html',
-  styleUrls: ['./User.component.css']
+  styleUrls: ['../../../../styles.css', './User.component.css']
 })
 export class UserComponent implements OnInit {
+
+  baseUrlCandidates = `https://localhost:7056/api/Candidate`;
+  baseUrlVoters = `https://localhost:7056/api/Voter`;
+  selectedCandidate: any;
+  selectedVoter: any;
+  candidates: any = [];
   voters: any = [];
-  baseUrl = `https://localhost:7056/api/Voter`;
-  constructor(private http: HttpClient) { }
+  voteForm!: FormGroup;
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
-    let req = this.http.get(this.baseUrl);
+    let req = this.http.get<any>(this.baseUrlCandidates);
     req.subscribe({
-      next: (data) => this.voters = data,
+      next: (data) => { this.candidates = data },
+      error: (err) => console.log(err)
+    });
+    let req2 = this.http.get<any>(this.baseUrlVoters);
+    req2.subscribe({
+      next: (data) => {
+        data.forEach((item: any) => {
+          if (item.voteCasted == null || false) {
+            this.voters.push(item);
+          }
+        })
+      },
       error: (err) => console.log(err)
     });
     console.log(this.voters);
-    
+
+
+    this.voteForm = new FormGroup({
+      candidate: new FormControl('', Validators.required),
+      voter: new FormControl('', Validators.required)
+    });
+
+
   }
+
+  navigateToCandidates() {
+    this.router.navigateByUrl('/user/candidates');
+  }
+  navigateToResults() {
+    this.router.navigateByUrl('/user/results');
+  }
+
+  castVote() {
+    let vote: any;
+    let candidate: any;
+    let voter: any;
+    if (this.voteForm.invalid) {
+      alert('Invalid data provided.');
+    }
+    else {
+      let ctrl = this.voteForm.controls;
+      vote = {
+        casted: true,
+        votedBy: ctrl['voter'].getRawValue(),
+        votedFor: ctrl['candidate'].getRawValue()
+      };
+      console.log(vote);
+    }
+    //extract candidate from Vote
+    this.candidates.forEach((item: any) => {
+      if (item.id == vote.candidate) {
+        candidate = item;
+      }
+    });
+
+    //extract voter from Vote
+    this.voters.forEach((item: any) => {
+      if (item.id == vote.voter) {
+        voter = item;
+      }
+    });
+
+    console.log(candidate, voter);
+  }
+
 
 }
