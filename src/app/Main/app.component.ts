62dcb4d5e580439f17cf1428d0ac680f72bf2370
ahadddd <ahad.shehbaz19@gmail.com>
2023-06-02 00:40:51 +0500

    Navbar settings configured

--- src/app/Main/app.component.ts
+++ src/app/Main/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private router: Router) {}
   title = 'VotingApp-Frontend';
+  app = true;
+
+
+  NavigateToAdmin() {
+    this.app = false;
+    this.router.navigate(['/admin']);
+  }
+  NavigateToUser() {
+    this.app = false;
+    this.router.navigate(['/user']);
+  }
 }
